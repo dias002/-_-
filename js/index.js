9fe5eb35736de7bbc7ac0a89e262d838f5e3d02a
dias002 <95093334+dias002@users.noreply.github.com>
2022-10-23 05:34:05 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-  AOS.init();
+AOS.init();
+const modalButton = document.querySelector('.modal_btn')
+const modalWindow = document.querySelector('#modal-1')
+const phoneBtn = document.querySelector('.phone-button')
+const teamCards = document.querySelector('.team')
+const teamBtn = document.querySelector('.team_btn')
+
+teamBtn.addEventListener('click', function(){
+    teamCards.classList.toggle('active')
+    console.log(teamCards)
+})
+modalButton.addEventListener('click', function(){
+    modalWindow.classList.remove('hidden')
+    phoneBtn.classList.add('hidden')
+})
+modalWindow.addEventListener('click', function(){
+    modalWindow.classList.add('hidden')
+    phoneBtn.classList.remove('hidden')
+})
+
+const regBox = document.querySelector('#regBox').addEventListener('click', function(event){
+    event.stopPropagation();
+})
